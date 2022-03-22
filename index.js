e4af2e05c89a73f8984069f630fb82463c588136
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-03-22 20:42:29 +0000

    link page to where readme is deployed

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// require modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer'); 
+
+// linking to page where the README is developed 
+const generatePage = require('./utils/generateMarkdown.js');
 // array of questions for user
 const questions = () => {
     // using inquirer to prompt questions to user 
